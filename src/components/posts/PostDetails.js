b1c9c30257d293ffba1513ffcb9467e080611e8a
Jeremy Myers <echelonical@gmail.com>
2022-10-21 15:02:14 -0500

    Testing 123

--- src/components/posts/PostDetails.js
+++ src/components/posts/PostDetails.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { getPostById, getPosts } from "../../managers/PostsManger"
+import { Navigate, useParams } from "react-router-dom"
+import { deletePost, getPostById } from "../../managers/PostsManger"
 
 export const PostDetails = () => {
 
@@ -26,14 +26,18 @@ export const PostDetails = () => {
     */
 
     return <article className="post_">
-
         <h2>Post Details:</h2>
         <section className="postDetails">
-            <div className="details__title">Title: {posts.title}</div>
-            <div className="details__author--name">Author Named: {posts.user_id}</div>
-            <div className="details__category">Category: {posts.category_id}</div>
-            <div className="details__publication--date">Publication Date: {posts.publication_date}</div>
-            <div className="details__content">Content: {posts.content}</div>
+            <div>
+                <div className="details__title">Title: {posts.title}</div>
+                <div className="details__author--name">Author Named: {posts.user_id}</div>
+                <div className="details__category">Category: {posts.category_id}</div>
+                <div className="details__publication--date">Publication Date: {posts.publication_date}</div>
+                <div className="details__content">Content: {posts.content}</div>
+            </div>
+            <button className="btn_delete-post" onClick={() => deletePost(posts.id)}>DELETE</button>
+            <button className="btn_delete-post" onClick={() => deletePost(posts.id)}>DELETE</button>
+            <button className="btn_delete-post" onClick={() => deletePost(posts.id)}>DELETE</button>
         </section>
 
     </article>
--- src/managers/PostsManger.js
+++ src/managers/PostsManger.js
@@ -7,3 +7,9 @@ export const getPostById = (id) => {
     return fetch(`http://localhost:8088/posts/${id}`)
         .then(res => res.json())
 }
+
+export const deletePost = (postId) => {
+    return fetch(`http://localhost:8088/posts/${postId}`, {
+        method: "DELETE"
+    })
+}
