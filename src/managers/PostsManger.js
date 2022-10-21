52cd54c02bc4bba3c284fbeaa21425b5c91336a7
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-10-21 15:29:55 -0500

    Edit Post

--- src/managers/PostsManger.js
+++ src/managers/PostsManger.js
@@ -7,3 +7,13 @@ export const getPostById = (id) => {
     return fetch(`http://localhost:8088/posts/${id}`)
         .then(res => res.json())
 }
+
+export const saveEditedPost = (post) => {
+    return fetch(`http://localhost:8088/posts/${post.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(post)
+    })
+}
