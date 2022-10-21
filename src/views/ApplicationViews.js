7e963a5318a7c21b8730d5817cc97fc539230906
Faith Cox <fcox662@gmail.com>
2022-10-21 10:10:14 -0400

    merging with main- team additions

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
-
-import { CategoryList } from "../components/categories/CategoryList"
+// import { CategoryContainer } from "../components/categories/CategoryContainer"
 import { AllPosts } from "../components/posts/PostList"
 import { TagList } from "../components/tags/TagList"
 import { Authorized } from "./Authorized"
+import { PostDetails } from "./components/posts/PostDetails"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
@@ -13,13 +13,12 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/login" element={<Login setToken={setToken} />} />
       <Route path="/register" element={<Register setToken={setToken} />} />
       <Route element={<Authorized token={token} />}>
-        
-          <Route path="/tags" element={<TagList/>} />
 
-          <Route path="/categories" element={<CategoryList/>} />
+        {/* <Route path="/categories" element={<CategoryContainer/>} /> */}
+        <Route path="/tags" element={<TagList />} />
+        <Route path="/posts" element={<AllPosts />} />
+        <Route path="/posts/:postId" element={<PostDetails />} />
 
-          <Route path="/posts" element={<AllPosts/>} />
-        
 
       </Route>
     </Routes>
