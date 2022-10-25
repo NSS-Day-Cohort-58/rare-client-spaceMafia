2b87fa57e7b3991d1397f162e521e98c037d9c9a
Jeremy Myers <echelonical@gmail.com>
2022-10-25 11:31:47 -0500

    updates to TagManager, TagEdit.js Module created

--- src/managers/TagManager.js
+++ src/managers/TagManager.js
@@ -12,3 +12,13 @@ export const createTag = (tag) => {
         body: JSON.stringify(tag)
     })
 }
+
+export const saveEditedTag = (tag) => {
+    return fetch(`http://localhost:8088/posts/${tag.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(tag)
+    })
+}
