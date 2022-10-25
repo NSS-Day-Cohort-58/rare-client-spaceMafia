414287a3875f89af17f0f0030328350d2305766f
Jeremy Myers <echelonical@gmail.com>
2022-10-25 12:27:32 -0500

    styling updates

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -16,13 +16,13 @@ export const TagList = () => {
         []
     )
 
-    return <section className="tags__list">
+    return <section className="tags__list box">
         <h2 className="tag__header title">Tags</h2>
         {
             tags.map(tag => {
-                return <div key={`tag--${tag.id}`} className="tag columns level" id="tags">
+                return <div key={`tag--${tag.id}`} className="tag columns" id="tags">
                     <div className="column"><h3 className="subtitle is-5 is-marginless">{tag.label}</h3></div>
-                    <div className="tag__buttons column level-right">
+                    <div className="tag__buttons column">
                         <button className="tag__button button is-small is-success " onClick={() => navigate(`/tags/${tag.id}`)}>Edit</button>
                         <button className="tag__button button is-small is-danger">Delete</button>
                     </div>
