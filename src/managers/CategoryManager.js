e4d09e56348f43b75da519241a1a28f3c11d4913
Faith Cox <fcox662@gmail.com>
2022-11-14 16:39:58 -0500

    fixed get request for categories in the manager

--- src/managers/CategoryManager.js
+++ src/managers/CategoryManager.js
@@ -1,10 +1,16 @@
 export const getCategories = () => {
-    return fetch(`http://localhost:8088/categories`)
+    return fetch(`http://localhost:8000/categories`, {
+        headers:{
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            //"Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
         .then(res => res.json())
 }
 
 export const createCategory = (category) => {
-    return fetch("http://localhost:8088/categories", {
+    return fetch("http://localhost:8000/categories", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
