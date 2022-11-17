8ae1928448836458caef4405bfc95ecdbd83c7c0
Faith Cox <fcox662@gmail.com>
2022-11-17 12:05:54 -0500

    new client main

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react"
-import { getCategories } from "../../managers/CategoryManager"
+import { Link, useNavigate } from "react-router-dom"
+import { getCategories, deleteCategory } from "../../managers/CategoryManager"
 import './CategoryList.css'
 
 export const CategoryList = () => {
     const [categories, setCategories] = useState([])
+    const navigate = useNavigate()
+
+    const localForumUser = localStorage.getItem("forum_user")
+    const forumUserObject = JSON.parse(localForumUser)
 
     useEffect (
         () => {
@@ -13,10 +18,19 @@ export const CategoryList = () => {
         []
     )
 
+    const categoryDelete = (evt, category) => {
+        // whenever confirmed by clicking OK/Cancel window.confirm() returns boolean 
+        let text = 'Are you sure you want to delete this category?'
+        window.confirm(text)
+            ? deleteCategory(category.id).then(() => navigate("/categories"))
+            : <></>
+    }
+
     return <section> <div className="category_title">Category List</div>
         {categories.map(category => {
             return <div key={`category--${category.id}`} className="category">
                 <h3 className="category_label">{category.label}</h3>
+        <button className="category__button button is-small is-responsive is-danger" onClick={(evt) => categoryDelete(evt, category).then(() => {window.location.reload()})}>Delete</button>     
                 <div>
                 </div>
             </div>
